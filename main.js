672fda59b28788117afb315b7f01e36b8c915d40
willly_droof <droofwilly@gmail.com>
2023-05-09 15:56:19 -0400

    fix open bottom sheet

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 
-const openSheet = document.querySelector(".draggable-area")
-
+const openSheet = document.querySelector(".draggable-content")
+const openCLckSheet = document.querySelector(".controls-sheet")
+const contentSheet = document.querySelector(".content-sheet")
 const sheet = document.querySelector(".sheet")
-const draggable = document.querySelector(".draggable-area")
+const draggable = document.querySelector(".container-sheet")
 
 let sheetHeight = 0
+let dragPosition = 0
+
 const SheetOpen = () => {
     sheet.classList.add("active")
 }
 const SheetClose = () => {
     sheet.classList.remove("active")
 }
+const contentTop = () => {
+    contentSheet.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
 
 const setSheetHeight = (value) => {
+    if (contentSheet.scrollTop > 3) return
 
     sheetHeight = Math.max(0, Math.min(100, value))
-    if (sheetHeight <= 5) return sheet.style.height = `${5}vh`
+    if (sheetHeight <= 5) return sheet.style.height = `${40}px`
 
-    sheet.style.height = `${sheetHeight}vh`
+    sheet.style.height = `${sheetHeight}%`
 
-    if (sheetHeight >= 100) {
-        sheet.classList.add("fullscreen")
-    } else {
-        sheet.classList.remove("fullscreen")
+    if (sheetHeight > 75) {
+        contentSheet.style.overflowY = "auto";
+        SheetOpen()
+    }
+    if (sheetHeight <= 75) {
+        contentTop()
+        contentSheet.style.overflowY = "hidden";
     }
 }
 
-openSheet.addEventListener("click", () => {
+openCLckSheet.addEventListener("click", () => {
     let height = Math.min(50, 720 / window.innerHeight * 100)
+    contentTop()
     if (sheet.classList.contains("active")) {
-        setSheetHeight(5)
         height = 5
+        setSheetHeight(5)
         SheetClose()
     } else {
         SheetOpen()
@@ -38,22 +52,9 @@ openSheet.addEventListener("click", () => {
     }
 })
 
-
-const isFocused = element => document.activeElement === element
-
-window.addEventListener("keyup", (event) => {
-    const isSheetElementFocused =
-        sheet.contains(event.target) && isFocused(event.target)
-
-    if (event.key === "Escape" && !isSheetElementFocused) {
-        SheetClose(false)
-    }
-})
-
-const touchPosition = (event) =>
-    event.touches ? event.touches[0] : event
-
-let dragPosition
+const touchPosition = (event) => {
+    return event.touches ? event.touches[0] : event
+}
 
 const onDragStart = (event) => {
     dragPosition = touchPosition(event).pageY
@@ -62,12 +63,10 @@ const onDragStart = (event) => {
 }
 
 const onDragMove = (event) => {
-    if (dragPosition === undefined) return
-
+    if (!dragPosition) return
     const y = touchPosition(event).pageY
     const deltaY = dragPosition - y
     const deltaHeight = deltaY / window.innerHeight * 100
-
     setSheetHeight(sheetHeight + deltaHeight)
 
     if (y > 950) return dragPosition = 950
@@ -75,24 +74,31 @@ const onDragMove = (event) => {
 }
 
 const onDragEnd = () => {
-    dragPosition = undefined
+    dragPosition = 0
     sheet.classList.remove("not-selectable")
     draggable.style.cursor = document.body.style.cursor = ""
 
     if (sheetHeight < 25) {
         setSheetHeight(5)
+        contentTop()
         SheetClose()
-    } else if (sheetHeight > 75) {
-        setSheetHeight(90)
+    } else if (sheetHeight > 70) {
+        setSheetHeight(100)
+        contentSheet.style.overflowY = "auto";
         SheetOpen()
     } else {
         setSheetHeight(50)
+        contentSheet.style.overflowY = "hidden";
         SheetOpen()
     }
+  
 }
 
 draggable.addEventListener("mousedown", onDragStart)
 draggable.addEventListener("touchstart", onDragStart)
+
+openSheet.addEventListener("mousedown", onDragStart)
+openSheet.addEventListener("touchstart", onDragStart)
 
 window.addEventListener("mousemove", onDragMove)
 window.addEventListener("touchmove", onDragMove)
